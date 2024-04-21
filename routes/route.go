8507b1f59f55080f9f327a3c2ec0e490c61bb7b1
kodlitecom <nandaaprilia605@gmail.com>
2024-04-21 13:08:47 +0700

    first commit

---
+++ routes/route.go
@@ -0,0 +1,17 @@
+package routes
+
+import (
+	"apiantr/controllers"
+	"apiantr/middlewares"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func Routes(r *fiber.App) {
+	ant := r.Group("/api")
+
+	ant.Get("/", middlewares.AuthMiddleware, controllers.Index)
+	ant.Get("/:id", middlewares.AuthMiddleware, controllers.Show)
+	ant.Post("/:id", middlewares.AuthMiddleware, controllers.Update)
+	ant.Post("/reset/:id", middlewares.AuthMiddleware, controllers.Reset)
+}
